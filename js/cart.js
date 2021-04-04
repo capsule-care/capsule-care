9e1e63cc1d48125ce3d6c2a6838c2c14d3091bfe
mohammadabuhatab <abu123hatab@gmail.com>
2021-04-04 22:42:59 +0300

    creat a constructer and table

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,32 @@
+'use strict';
+function Cart2(name,price,quantity){
+  this.name=name;
+  this.price=price;
+  this.quantity=quantity;
+  this.total=this.price*this.quantity;
+}
+Cart2.prototype.render=function(){
+  const productName = document.createElement('td');
+  cartTable.appendChild(productName);
+  productName.textContent = this.name;
+  const individualPrice = document.createElement('td');
+  cartTable.appendChild(individualPrice);
+  individualPrice.textContent = this.price;
+  const quantity = document.createElement('td');
+  cartTable.appendChild(quantity);
+  quantity.textContent = this.quantity;
+  const totalPrice = document.createElement('td');
+  cartTable.appendChild(totalPrice);
+  totalPrice.textContent = this.total;
+  const newLine = document.createElement('tr');
+  cartTable.appendChild(newLine);
+};
 
-const td1El = document.createElement('td');
-cartTable.appendChild(td1El);
-td1El.textContent = "this.name";
-const td2El = document.createElement('td');
-cartTable.appendChild(td2El);
-td2El.textContent = "this.name";
-const td3El = document.createElement('td');
-cartTable.appendChild(td3El);
-td3El.textContent = "this.name";
-const td4El = document.createElement('td');
-cartTable.appendChild(td4El);
-td4El.textContent = "this.name";
+let cartName = ['moh','ab'];
+let prices=[3,4];
+let quantitys=[4,5];
+
+for (let i = 0; i < cartName.length; i++) {
+  let newCart = new Cart2 (cartName[i], prices[i], quantitys[i]);
+    newCart.render();
+}
